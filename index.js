30765a0517274c0b21c8a49607560f82fd71bd6b
growwithboss <growwithboss@gmail.com>
2025-12-22 16:24:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+﻿const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({ message: "Backend is running successfully!" });
+});
+
+app.listen(PORT, () => {
+    console.log("Server is listening on port " + PORT);
+});
